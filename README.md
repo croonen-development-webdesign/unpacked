b7bec3aff6af14e9f9ec82d2138a71e05be089cc
jan croonen <info@jcroonen.nl>
2024-04-30 11:43:12 +0200

    refact

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # unpacked
  List and inspect unpacked sources of downloaded browser extensions.
+
+ide: run WebStorm (or something) with the unpacked extension
+
+reveal: open Finder (or something)
+
--- config.js
+++ config.js
@@ -4,7 +4,6 @@ const toolboxdir = homedir + '/Library/Application Support/JetBrains/Toolbox';
 module.exports = {
     port: 3019,
     edge_extensions_dir: homedir + '/Library/Application Support/Microsoft Edge/Default/Extensions',
-    edge_extensions_list: homedir + '/Dev/node/unpacked/tmp/extensions-list2.json',
     idea_toolbox: {
         webstorm: toolboxdir + '/WebStorm',
         phpstorm: toolboxdir + '/PhpStorm',
